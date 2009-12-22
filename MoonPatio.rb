14b8decb64151eefe3720dab9fd32d5e143e4530
Vishnu Iyengar <pathsny@gmail.com>
2009-12-22 16:44:31 +0530

    script files

---
+++ MoonPatio.rb
@@ -0,0 +1,58 @@
+#!/opt/local/bin/ruby1.9
+#!/usr/bin/ruby
+require 'rubygems'
+require 'osax'
+require '32bit/standard_additions'
+require 'cgi'
+require 'net/http'
+
+# this class figures out what language the code is in. First by using the clue you've typed in
+# and if that doesn't work, by throwing up a dialog.
+class Language
+	def initialize(languages, osax)
+		@languages = languages
+		@osax = osax
+	end	
+
+	def contained?(name,abbrv)
+		return true if abbrv.empty?
+		return false if name.empty?
+		abbrv = abbrv[1..-1] if (name[0] == abbrv[0])
+		contained?(name[1..-1],abbrv)  
+	end	
+
+	def get_language(abbrv)
+		chosen = @languages.select{|name| con = contained?(name.upcase, abbrv)}
+		return chosen.first if chosen.size == 1 #only one supported language contains the abbreviation
+
+		exact = chosen.select{|lang| lang.size == abbrv.size}
+		return exact.first unless exact.empty? #only one supported language exactly matches the abbreviation
+
+		single_word = chosen.reject{|lang| lang.include?('+') || lang.include?(' ')}
+		return single_word.first if single_word.size == 1 #only one supported language is a single word that contains the abbreviation
+
+		chosen = @languages if chosen.empty?
+		return get_selection chosen
+	end	
+
+	def get_selection(languages)
+		result = @osax.choose_from_list(languages, :with_title => "Choose Language", 
+		:with_prompt => "which of these languages is the code in?", :OK_button_name => "ok", :multiple_selections_allowed => false)
+		result && result.first		
+	end	
+end	
+
+def get_http  
+	uri = URI.parse(`source ~/.bash_setproxy;echo $http_proxy`)
+	proxy_user, proxy_pass = uri.userinfo.split(/:/) if uri.userinfo
+	Net::HTTP::Proxy(uri.host,uri.port,proxy_user,proxy_pass)
+end	
+
+osax = OSAX::ScriptingAddition.new('osax', StandardAdditions)		
+http = get_http
+languages = Hash[*http.get(URI.parse('http://moonpatio.org/fastcgi/hpaste.fcgi/new')).scan(/<option [^>]*value=\"([^\"]*)\">([^>]*)</).collect(&:reverse).flatten]
+chosen = Language.new(languages.keys, osax).get_language ARGV[0].upcase
+exit unless chosen
+response = http.post_form URI.parse('http://moonpatio.org/fastcgi/hpaste.fcgi/save'), "content" => osax.the_clipboard, "author" => "path", 
+"language" => languages[chosen], "title" => ".", "channel" => ""
+osax.set_the_clipboard_to("http://moonpatio.org#{response["location"]}")
